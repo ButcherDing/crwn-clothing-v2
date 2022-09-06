a78f04a8a8ee0e3bd8259a9ed7b4321cb9bb981b
PengBird <cejed@protonmail.com>
2022-09-06 10:09:53 +0100

    added sign-in component and firebase utils, firebase libraries

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+// fragments are great because they don't actually appear as an element in the DOM when you actually load the page. Remember we need a top-level element when making our compenents, no siblings rule.
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo">Logo</CrwnLogo>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
