5438a28572b297ddbcad4b6ea3bcb5e26221909c
PengBird <cejed@protonmail.com>
2022-09-06 16:31:57 +0100

    created sign-up, form input, button components

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import "./sign-up-form.styles.scss";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  // console.log(formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+    // check passwords match
+    if (password !== confirmPassword) return alert("passwords do not match");
+    // get user auth
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      // pass displayname - was done differently in guide. Is this bad practice? Not sure why it would be... Initially I thought the same, pass in the displayname into the createUserDoc... function and somehow append it over there. But if object shapes are generic from Google, shouldn't we make them fit to that generic shape? Will this cause problems with user auth in future? We'll see...
+      user.displayName = displayName;
+      // create user
+      const userDocRef = await createUserDocumentFromAuth(user);
+      resetFormFields();
+    } catch (err) {
+      if (err.code === "auth/email-already-in-use") {
+        alert("Can't create a new account - that email is already in use.");
+      } else {
+        console.error("problem creating new user:", err.message);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+
+        <FormInput
+          label="email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+        <Button type="google">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,33 @@
 import {
-  signInWithGooglePopup,
+  auth,
   createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component.jsx";
+
 const SignIn = () => {
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth);
+  //   if (response);
+  // }, []);
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    console.log(user);
-    const userDocRef = await createUserDocumentFromAuth(user);
+    const userDocRef = createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
     </div>
   );
 };
