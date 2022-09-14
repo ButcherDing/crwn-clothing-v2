96a133ba1efd50a3c91f0469a0754118410a1902
PengBird <cejed@protonmail.com>
2022-09-14 13:25:37 +0100

    shop routes working

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,18 @@
 import ProductCard from "../product-card/product-card.component";
 import "./category-preview.styles.scss";
+import { Link } from "react-router-dom";
 
 const CategoryPreview = ({ title, products }) => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link to={title} onClick={scrollToTop}>
+          <span className="title">{title.toUpperCase()}</span>
+        </Link>
       </h2>
       <div className="preview">
         {products
